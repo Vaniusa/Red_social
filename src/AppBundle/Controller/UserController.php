5d21523b4e167fdf6297d95dc93af48847507d90
Vaniusa <ionsaitan1980@gmail.com>
2017-08-03 12:02:08 +0200

    4_commit_s6_c22

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,11 +4,23 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use BackendBundle\Entity\User;
+use AppBundle\Form\RegisterType;
 
 class UserController extends Controller
 {
     public function loginAction(Request $request)
     {
         return $this->render('AppBundle:User:login.html.twig');
+    }
+
+    public function registerAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        return $this->render('AppBundle:User:register.html.twig', array(
+            "form" => $form->createView()
+        ));
     }
 }
