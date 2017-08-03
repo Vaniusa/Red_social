77235dc1864db6bb6ddcfed558321749f128d91b
Vaniusa <ionsaitan1980@gmail.com>
2017-08-03 20:33:38 +0200

    5_commit_s6_c25

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,13 +2,22 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\RegisterType;
+use BackendBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use BackendBundle\Entity\User;
-use AppBundle\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class UserController extends Controller
 {
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new Session();
+    }
+
     public function loginAction(Request $request)
     {
         return $this->render('AppBundle:User:login.html.twig');
@@ -18,9 +27,66 @@ class UserController extends Controller
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $query = $em->createQuery('SELECT u FROM BackendBundle:User u WHERE u.email = :email OR u.nick = :nick')
+                            ->setParameter('email', $form->get("email")->getData())
+                            ->setParameter('nick', $form->get("nick")->getData());
+
+                $user_isset = $query->getResult();
+
+                if (count($user_isset) == 0) {
+                    $factory = $this->get("security.encoder_factory");
+                    $encoder = $factory->getEncoder($user);
+                    $password = $encoder->encodePassword($form->get("password")->getData(), $user->getSalt());
+
+                    $user->setPassword($password);
+                    $user->setRole("ROLE_USER");
+                    $user->setImage(null);
+
+                    $em->persist($user);
+                    $flush = $em->flush();
+
+                    if ($flush == null) {
+                        $status = "Te has registrado correctamente";
+                        $this->session->getFlashBag()->add("status", $status);
+                        return $this->redirect("login");
+                    } else {
+                        $status = "No te has registrado correctamente";
+                    }
+                } else {
+                    $status = "Este usuario ya existe !!";
+                }
+            } else {
+                $status = "No te has registrado correctamente !!";
+            }
+
+            $this->session->getFlashBag()->add("status", $status);
+        }
 
         return $this->render('AppBundle:User:register.html.twig', array(
             "form" => $form->createView()
         ));
+    }
+
+    public function nickTestAction(Request $request)
+    {
+        $nick = $request->get("nick");
+
+        $em = $this->getDoctrine()->getManager();
+        $user_repo = $em->getRepository("BackendBundle:User");
+        $user_isset = $user_repo->findOneBy(array("nick" => $nick));
+
+        $result = "used";
+        if (count($user_isset) >= 1 && is_object($user_isset)) {
+            $result = "used";
+        } else {
+            $result = "unused";
+        }
+
+        return new Response($result);
     }
 }
