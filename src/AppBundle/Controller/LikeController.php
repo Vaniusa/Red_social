65a694bdb136de6a9a92f31123c1515446f3a9e1
Vaniusa <ionsaitan1980@gmail.com>
2017-08-15 18:17:34 +0200

    19_commit_s12_c68

--- src/AppBundle/Controller/LikeController.php
+++ src/AppBundle/Controller/LikeController.php
@@ -48,4 +48,30 @@ class LikeController extends Controller
         }
         return new Response($status);
     }
+
+    public function likesAction(Request $request, $nickname = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        if ($nickname != null) {
+            $user_repo = $em->getRepository('BackendBundle:User');
+            $user = $user_repo->findOneBy(array('nick' => $nickname));
+        } else {
+            $user = $this->getUser();
+        }
+
+        if (empty($user) || !is_object($user)) {
+            return $this->redirect($this->generateUrl('home_publication'));
+        }
+
+        $user_id = $user->getId();
+        $dql = "SELECT l FROM BackendBundle:Like l WHERE l.user = $user_id ORDER BY l.id DESC";
+        $query = $em->createQuery($dql);
+
+        $paginator = $this->get('knp_paginator');
+        $likes = $paginator->paginate($query, $request->query->getInt('page', 1), 5);
+
+        return $this->render('AppBundle:Like:likes.html.twig', array(
+            'user' => $user, 'pagination' => $likes));
+    }
 }
